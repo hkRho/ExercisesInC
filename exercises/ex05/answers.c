0bc2ca0b1e4c56b4cff6d3f1c9cd0b3c3d3cc59f
hkRho <hrho@olin.edu>
2020-03-05 19:43:03 -0500

    finish 'Structs, unions and bitfields' part of the hw

---
+++ exercises/ex05/answers.c
@@ -0,0 +1,14 @@
+/*
+                    175.370000 ms 	 dummy
+                    505.205000 ms 	 dummy2
+                    542.345000 ms 	 random_float
+                    595.657000 ms 	 my_random_float
+                    1485.731000 ms 	 my_random_float2
+                    539.829000 ms 	 random_float
+                    2083.817000 ms 	 my_random_double
+                    600.033000 ms 	 random_double
+3. The random number generator "dummy" is the fastest with 175.370000 ms.
+
+4. "random_double" is faster than "my_random_double". "my_random_double" takes
+    2083.817000 ms and the "random_double" takes 600.033000 ms.
+*/
